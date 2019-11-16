dd73d6b3f34fbda986e9d760085f7995402c21ae
viveksingh-lp <viveksingh.lp@gmail.com>
2019-11-16 18:19:58 +0530

    Update mongoose connection method

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Import routes
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 // Connect to DB
-mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true}, () => {
-    console.log('Connected to DB')
-});
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Database Connection Successful!'))
+.catch(err => console.error(err));
 
-// Import routes
-const authRoute = require('./routes/auth');
+// Middlewares
+app.use(express.json());
 
 // Route middleware
 app.use('/api/user', authRoute);
