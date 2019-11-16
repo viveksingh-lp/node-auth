5af80743f5332ae3890f1c6d3beeaec03f8d5bcb
viveksingh-lp <viveksingh.lp@gmail.com>
2019-11-16 16:20:53 +0530

    Add database connection

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true}, () => {
+    console.log('Connected to DB')
+});
 
 // Import routes
 const authRoute = require('./routes/auth');
