38b6573331e6d21257ccf93b4774576f1d532cc6
viveksingh-lp <viveksingh.lp@gmail.com>
2019-11-23 11:58:05 +0530

    Update 'auth.js'

--- routes/auth.js
+++ routes/auth.js
@@ -63,7 +63,7 @@ router.post('/login', async (req, res) => {
 
   // Generate token
   const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
-  res.header('auth-token', token).send(token);
+  res.header('Authorization', token).send(token);
 });
 
 module.exports = router;
